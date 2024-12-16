1a55733c73e0791efe9c7e975ca04cdd86fac74c
Rohit Singh <rsingh.py@gmail.com>
2024-12-16 15:24:44 +0530

    folder structure

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import UserMixin
 db = SQLAlchemy()
 
 #to be changed as per the ER Diagram Provided by the team
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class User(UserMixin,db.Model):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     
