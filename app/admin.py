9a24cdab6b89d0b8688ebf09351b5f0b0e3d1cd5
Rohit Singh <rsingh.py@gmail.com>
2024-12-24 20:34:59 +0530

    Update admin.py
    try to seperate the forms

--- app/admin.py
+++ app/admin.py
@@ -6,38 +6,11 @@ from wtforms.validators import DataRequired, InputRequired, NumberRange
 from flask_login import login_required, current_user
 from .models import db, User, Product
 from .decorators import is_admin
+from .forms import AddItemForm
+
 
 # Define the admin blueprint
 admin = Blueprint('admin', __name__)
-
-# Define the form
-class AddItemForm(FlaskForm):
-    """Form for adding items to the shop."""
-    name = StringField('Name', validators=[DataRequired(message="Name is required.")])
-
-    description = StringField('Description', validators=[DataRequired(message="Description is required.")])
-
-    price = FloatField('Price', validators=[
-        InputRequired(message="Price is required."),
-        NumberRange(min=0, message="Price must be a positive number.")
-    ])
-
-    stock_quantity = IntegerField('Stock Quantity', validators=[
-        InputRequired(message="Stock quantity is required."),
-        NumberRange(min=0, message="Stock quantity must be a non-negative number.")
-    ])
-
-    brand = StringField('Brand', validators=[DataRequired(message="Brand is required.")])
-    
-    category = StringField('Category', validators=[DataRequired(message="Category is required.")])
-    
-    updated_at = DateTimeField('Updated At')  # Optional; pre-filled if not provided
-    
-    rating = FloatField('Rating', validators=[
-        NumberRange(min=0, max=5, message="Rating must be between 0 and 5.")
-    ])
-    
-    ratting = StringField('Ratting', validators=[DataRequired(message="Ratting is required.")])
 
 # Define the route to add shop items
 @admin.route('/add-shop-items', methods=['POST'])
