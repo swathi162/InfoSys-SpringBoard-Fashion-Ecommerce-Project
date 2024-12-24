1fda00cad8a43f082030d9a3340717fd84c63fbe
Rohit Singh <rsingh.py@gmail.com>
2024-12-24 20:37:54 +0530

    Update forms.py
    added add item form to forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, SelectField, FloatField, IntegerField, DateTimeField, FileField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, InputRequired, NumberRange
+from datetime import datetime
 
 # User Registration Form
 class RegistrationForm(FlaskForm):
@@ -44,3 +45,31 @@ class ChangePasswordForm(FlaskForm):
     new_password = PasswordField('New Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('new_password')])
     submit = SubmitField('Change Password')
+
+class AddItemForm(FlaskForm):
+    """Form for adding items to the shop."""
+    name = StringField('Name', validators=[DataRequired(message="Name is required.")])
+    
+    description = StringField('Description', validators=[DataRequired(message="Description is required.")])
+    
+    price = FloatField('Price', validators=[
+        InputRequired(message="Price is required."),
+        NumberRange(min=0, message="Price must be a positive number.")
+    ])
+    
+    stock_quantity = IntegerField('Stock Quantity', validators=[
+        InputRequired(message="Stock quantity is required."),
+        NumberRange(min=0, message="Stock quantity must be a non-negative number.")
+    ])
+    
+    brand = StringField('Brand', validators=[DataRequired(message="Brand is required.")])
+    
+    category = StringField('Category', validators=[DataRequired(message="Category is required.")])
+    
+    updated_at = DateTimeField('Updated At', default=datetime.utcnow)  # Optional, auto-populates with current time
+    
+    rating = FloatField('Rating', validators=[
+        NumberRange(min=0, max=5, message="Rating must be between 0 and 5.")
+    ])
+    
+    ratting = StringField('Ratting', validators=[DataRequired(message="Ratting is required.")])
